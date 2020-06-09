f3d7eee4b0a3cb8ad4cce83fdf971cdef75ba1f0
Ibrahim <ibrahim6a@yahoo.co.uk>
2020-06-09 10:18:59 +0100

    Updates

---
+++ precache-manifest.e8bb42e2e33ff23b5a8da81dd82c2cdd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92ead00fbe910d1f8d5e6b150e439c1",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "398e931bc58600c5249c",
+    "url": "/todo-app/static/js/2.3b3404c3.chunk.js"
+  },
+  {
+    "revision": "4503d799b4f3469a5a02",
+    "url": "/todo-app/static/js/main.01541276.chunk.js"
+  },
+  {
+    "revision": "21f7da94245515307336",
+    "url": "/todo-app/static/js/runtime-main.a3a8951a.js"
+  }
+]);
